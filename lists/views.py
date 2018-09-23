2e12f695cfec180607ea4ad5ec709fb6e6ed5331
IgnatiusFlo <fireflo@tutanota.com>
2018-09-23 17:19:56 +0100

    Created a home.html file and asked the view to render it. Added lists to installed apps

--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do</title><h1>To-Do</h1></html>')
+    return render(request, 'home.html')
